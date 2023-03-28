d962f98a97de465105f467b7d5e1b38b4f399fc9
Saurav Kumar <88251261+Sauravgem92@users.noreply.github.com>
2023-03-28 14:06:19 +0530

    Create Address.java

---
+++ Week 4/Day 4/Concept of Classes and Objects and Encapsulation/1.Customer Address/Address.java
@@ -0,0 +1,55 @@
+package com.lti;
+
+public class Address {
+	private String street, city, country;
+	private int pincode;
+
+	public Address() {
+		super();
+	}
+
+	public Address(String street, String city, int pincode, String country) {
+		super();
+		this.street = street;
+		this.city = city;
+		this.country = country;
+		this.pincode = pincode;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public int getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+
+	void displayAddress() {
+		System.out.println("Street: " + street + "\nCity: " + city + "\nPincode: " + pincode + "\nCountry: " + country);
+	}
+
+}
