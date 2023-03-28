1869eb306200ba7104e70b6d1bdc1965a02db947
Saurav Kumar <88251261+Sauravgem92@users.noreply.github.com>
2023-03-29 00:09:37 +0530

    Create Shape.java

---
+++ Week 4/Day 5/Object Relationship and Polymorphism/5.Area of shape/Shape.java
@@ -0,0 +1,28 @@
+
+public class Shape
+{
+	protected  Double area;
+    
+    public Shape()
+    {
+    }
+    public Shape(Double area)
+    {
+        this.area=area;
+    }
+    
+    public void setArea(Double area)
+    {
+        this.area=area;
+    }
+    public Double getArea()
+    {
+        return this.area;
+    }
+    
+    public void computeArea()
+    {
+      this.setArea(0.0);
+    }
+}
+
