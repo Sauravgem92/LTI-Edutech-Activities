31459793584b8469638afb6158272b3d1f70c936
Saurav Kumar <88251261+Sauravgem92@users.noreply.github.com>
2023-03-28 15:28:49 +0530

    Create Player.java

---
+++ Week 4/Day 4/Concept of Classes and Objects and Encapsulation/5.Getters and setters/Player.java
@@ -0,0 +1,52 @@
+public class Player {
+	private String name;
+	private int age;
+	private String country;
+	
+	
+	
+	
+	public Player() {
+		super();
+	}
+
+
+	public Player(String name, int age, String country) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.country = country;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
+   
