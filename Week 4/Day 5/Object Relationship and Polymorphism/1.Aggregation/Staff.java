2ebb9d271b69d7186076e0720f9bf69c0afd22ef
Saurav Kumar <88251261+Sauravgem92@users.noreply.github.com>
2023-03-28 21:35:20 +0530

    Create Staff.java

---
+++ Week 4/Day 5/Object Relationship and Polymorphism/1.Aggregation/Staff.java
@@ -0,0 +1,28 @@
+
+public class Staff {
+	private String staffName;
+	private String designation;
+
+	public Staff(String staffName, String designation) {
+		super();
+		this.staffName = staffName;
+		this.designation = designation;
+	}
+
+	public String getStaffName() {
+		return staffName;
+	}
+
+	public void setStaffName(String staffName) {
+		this.staffName = staffName;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+}
