426b95014e58c8c0ea880d9da581f09c31e37911
Saurav Kumar <88251261+Sauravgem92@users.noreply.github.com>
2023-03-28 14:47:29 +0530

    Create Customer.java

---
+++ Week 4/Day 4/Concept of Classes and Objects and Encapsulation/2.Customer Class/Customer.java
@@ -0,0 +1,39 @@
+
+public class Customer {
+    private String customerName;
+    private String customerEmail;
+    private String customerType;
+    private String customerLocation;
+    
+    
+	public Customer(String customerName, String customerEmail, String customerType, String customerLocation) {
+		super();
+		this.customerName = customerName;
+		this.customerEmail = customerEmail;
+		this.customerType = customerType;
+		this.customerLocation = customerLocation;
+	}
+
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+
+
+	public String getCustomerType() {
+		return customerType;
+	}
+
+
+	public String getCustomerLocation() {
+		return customerLocation;
+	}
+    
+    
+
+}
