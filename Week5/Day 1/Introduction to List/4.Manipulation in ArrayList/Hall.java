863d59b82acd67c4214025262e409b7921684544
Saurav Kumar <88251261+Sauravgem92@users.noreply.github.com>
2023-03-29 15:24:29 +0530

    Create Hall.java

---
+++ Week5/Day 1/Introduction to List/4.Manipulation in ArrayList/Hall.java
@@ -0,0 +1,60 @@
+
+
+public class Hall {
+	private String name,contactNumber;
+	private double costPerDay;
+	private String ownerName;
+	
+	
+	public Hall(String name, String contactNumber, double costPerDay, String ownerName) {
+		super();
+		this.name = name;
+		this.contactNumber = contactNumber;
+		this.costPerDay = costPerDay;
+		this.ownerName = ownerName;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+
+	public double getCostPerDay() {
+		return costPerDay;
+	}
+
+
+	public void setCostPerDay(double costPerDay) {
+		this.costPerDay = costPerDay;
+	}
+
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+	
+	
+	
+
+}
