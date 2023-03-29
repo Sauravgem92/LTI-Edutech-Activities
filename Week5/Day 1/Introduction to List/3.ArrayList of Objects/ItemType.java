9c40db047e5b6499160bc4e6ba6eb5d4b1bcd56d
Saurav Kumar <88251261+Sauravgem92@users.noreply.github.com>
2023-03-29 13:44:46 +0530

    Create ItemType.java

---
+++ Week5/Day 1/Introduction to List/3.ArrayList of Objects/ItemType.java
@@ -0,0 +1,45 @@
+
+
+public class ItemType {
+	private String name;
+	private double deposit;
+	private double costPerDay;
+
+	public ItemType(String name, double deposit, double costPerDay) {
+		super();
+		this.name = name;
+		this.deposit = deposit;
+		this.costPerDay = costPerDay;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getDeposit() {
+		return deposit;
+	}
+
+	public void setDeposit(double deposit) {
+		this.deposit = deposit;
+	}
+
+	public double getCostPerDay() {
+		return costPerDay;
+	}
+
+	public void setCostPerDay(double costPerDay) {
+		this.costPerDay = costPerDay;
+	}
+
+	@Override
+	public String toString() {
+		System.out.printf("%-20s%-20s%-20s", this.getName(), this.getDeposit(), this.getCostPerDay());
+		return null;
+	}
+
+}
