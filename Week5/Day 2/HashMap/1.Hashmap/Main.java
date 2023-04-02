1d8571c0e4e332af887abedd6c9dec082184213f
Saurav Kumar <88251261+Sauravgem92@users.noreply.github.com>
2023-04-02 21:23:21 +0530

    Create Main.java

---
+++ Week5/Day 2/HashMap/1.Hashmap/Main.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+
+		Map<Integer, Integer> map = new HashMap<>();
+	
+	    int n=sc.nextInt();
+		sc.nextLine();
+
+		for (int i = 0; i < n; i++) {
+			map.put(sc.nextInt(), sc.nextInt());
+		}
+
+		System.out.println(UserMainCode.getSumOfEven(map));
+	}
+
+}
