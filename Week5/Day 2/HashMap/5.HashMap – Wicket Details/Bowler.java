a0c6c123d7a695051ecb2403024a46c5f59fb90d
Saurav Kumar <88251261+Sauravgem92@users.noreply.github.com>
2023-04-03 12:45:36 +0530

    Create Bowler.java

---
+++ Week5/Day 2/HashMap/5.HashMap – Wicket Details/Bowler.java
@@ -0,0 +1,18 @@
+
+public class Bowler {
+	private String name;
+
+	public Bowler(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
