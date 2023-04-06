14cc7aecec144264418cdc8ba8259bd36db45669
Saurav Kumar <88251261+Sauravgem92@users.noreply.github.com>
2023-04-06 17:32:34 +0530

    Create Role.java

---
+++ Week5/Day 3/Comparator and Comparable Interfaces/3.Lanchester bank/Role.java
@@ -0,0 +1,22 @@
+
+public class Role {
+	private String name;
+
+	public Role() {
+		super();
+	}
+
+	public Role(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
