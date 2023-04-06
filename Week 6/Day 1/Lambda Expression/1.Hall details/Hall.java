3e696de0131afe0fc483e11f99d1b1bc238d682f
Saurav Kumar <88251261+Sauravgem92@users.noreply.github.com>
2023-04-06 22:22:43 +0530

    Create Hall.java

---
+++ Week 6/Day 1/Lambda Expression/1.Hall details/Hall.java
@@ -0,0 +1,56 @@
+
+public class Hall {
+
+	private String name;
+
+	private Double costPerDay;
+	private String owner;
+
+	public Hall() {
+	}
+
+	public Hall(String name, Double costPerDay, String owner) {
+
+		super();
+
+		this.name = name;
+
+		this.costPerDay = costPerDay;
+		this.owner = owner;
+
+	}
+
+	public String getName() {
+
+		return name;
+
+	}
+
+	public void setName(String name) {
+
+		this.name = name;
+
+	}
+
+	public Double getCostPerDay() {
+		return costPerDay;
+
+	}
+
+	public void setCostPerDay(Double costPerDay) {
+
+		this.costPerDay = costPerDay;
+
+	}
+
+	public String getOwner() {
+		return owner;
+
+	}
+
+	public void setOwner(String owner) {
+
+		this.owner = owner;
+	}
+
+}
