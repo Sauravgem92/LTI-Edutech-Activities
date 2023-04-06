695c01373e57a5935e19975f4627ad12298d7ae0
Saurav Kumar <88251261+Sauravgem92@users.noreply.github.com>
2023-04-06 17:31:25 +0530

    Create AccountType.java

---
+++ Week5/Day 3/Comparator and Comparable Interfaces/3.Lanchester bank/AccountType.java
@@ -0,0 +1,24 @@
+
+public class AccountType {
+	private String accountType;
+
+	public AccountType() {
+		super();
+	}
+
+	public AccountType(String accountType) {
+		super();
+		this.accountType = accountType;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	
+	
+
+}
