9bbe1b500b8067952922e340e627b42c16797c80
Saurav Kumar <88251261+Sauravgem92@users.noreply.github.com>
2023-04-07 21:45:02 +0530

    Update Main.java

--- Week5/Day 4/2.Display the box using Generic class/Main.java
+++ Week5/Day 4/2.Display the box using Generic class/Main.java
@@ -1,20 +1,17 @@
-   
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+	
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the values for length, width and depth : ");
-		Integer length = Integer.parseInt(br.readLine());
-		Integer width = Integer.parseInt(br.readLine());
-		Integer depth = Integer.parseInt(br.readLine());
-
-		Box<Integer> box = new Box<Integer>(length, width, depth);
-		box.print();
+		Integer len = sc.nextInt();
+		Integer wid = sc.nextInt();
+		Integer dep = sc.nextInt();
+		Box<Integer, Integer, Integer> b = new Box<Integer, Integer, Integer>(len, wid, dep);
+		b.print();
 	}
 
 }
