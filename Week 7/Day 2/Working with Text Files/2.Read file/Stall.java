0de4673fb06fb49d94566b82f596b539eacbd487
Saurav Kumar <88251261+Sauravgem92@users.noreply.github.com>
2023-04-11 20:06:59 +0530

    Create Stall.java

---
+++ Week 7/Day 2/Working with Text Files/2.Read file/Stall.java
@@ -0,0 +1,42 @@
+
+public class Stall {
+	private String name;
+	private String category;
+	private String owner;
+
+	public Stall() {
+		super();
+	}
+
+	public Stall(String name, String category, String owner) {
+		super();
+		this.name = name;
+		this.category = category;
+		this.owner = owner;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+}
